0226b0f79289d15ce35e104186908d44842e9938
HemantNegi42 <140361940+HemantNegi42@users.noreply.github.com>
2024-11-17 20:44:05 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "*", // Allows all origins
+  origin: ["http://localhost:5173", "https://careerwave.onrender.com"], // List allowed origins
+  credentials: true, // Allow credentials (cookies, authorization headers)
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 };
+
 app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3000;
